44933f43e9ed4ad97d81ccebb996a5a3f5bcd942
Kaspar Valk <valk.kaspar@gmail.com>
2020-12-08 17:32:32 +0200

    Movement between activities; ViewModel uses data from Room

---
+++ app/src/main/java/li/kta/espguard/room/EventEntity.kt
@@ -0,0 +1,13 @@
+package li.kta.espguard.room;
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "event")
+data class EventEntity(
+  @PrimaryKey(autoGenerate = true) var id: Int,
+  var deviceId: String?,
+  val event_time: Date?
+)
+
