9ed978295851d8d576233350373e5af108b24141
Oliver <38502906+Yleroimar@users.noreply.github.com>
2020-12-13 17:03:29 +0200

    MainActivity lateinits replaced with nullables due to npe paranoia

--- app/src/main/java/li/kta/espguard/MqttService.kt
+++ app/src/main/java/li/kta/espguard/MqttService.kt
@@ -3,11 +3,11 @@ package li.kta.espguard
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import kotlinx.coroutines.*
-import li.kta.espguard.activities.SettingsActivity
+import li.kta.espguard.activities.SettingsActivity.Companion.PREFERENCES_FIREBASE_TOKEN
+import li.kta.espguard.activities.SettingsActivity.Companion.getSharedPreferences
 import li.kta.espguard.room.LocalSensorDb
 import li.kta.espguard.room.SensorEntity
 import org.eclipse.paho.android.service.MqttAndroidClient
@@ -18,16 +18,14 @@ import org.eclipse.paho.client.mqttv3.MqttMessage
 import java.time.ZonedDateTime
 import java.util.*
 
-class MqttService(
-    private val context: Context,
-    private val sensors: Array<SensorEntity>
-) : MqttCallbackExtended {
+class MqttService(private val context: Context, private val sensors: Array<SensorEntity>)
+    : MqttCallbackExtended {
 
     companion object {
         private const val TAG = "MqttService"
         private const val SERVER_URI = "tcp://kta.li:1883"
 
-        private const val HEALTH_HEARTBEAT_INTERVAL = 15000L // ms
+        private const val HEALTH_HEARTBEAT_INTERVAL = 15_000L // ms
 
         /**
          * Status topic: Any published message by the node (incl. health check).
@@ -47,128 +45,115 @@ class MqttService(
         const val STATUS_RESPONSE_ACTION = "li.kta.status.response"
         const val STATUS_REQUEST_ACTION = "li.kta.status.request"
 
-        private lateinit var mqttService: MqttService
+        private var mqttService: MqttService? = null
 
         fun initializeMqttService(context: Context, sensors: Array<SensorEntity>) {
-            mqttService = MqttService(context, sensors)
-            mqttService.initialize()
-            mqttService.initializePeriodicHealthCheck()
+            mqttService = MqttService(context, sensors).apply {
+                initialize()
+                initializePeriodicHealthCheck()
+            }
+
             Log.i(TAG, "MqttService initialized")
         }
 
         fun destroyMqttService() {
-            mqttService.destroy()
+            mqttService?.destroy()
             Log.i(TAG, "MqttService destroyed")
         }
 
-        fun getInstance(): MqttService? {
-            if (this::mqttService.isInitialized)
-                return mqttService
-            return null
-        }
+        fun getInstance(): MqttService? = mqttService
     }
 
-    private lateinit var mqttClient: MqttAndroidClient
-    private lateinit var periodicHealthCheckJob: Job
+    private var mqttClient: MqttAndroidClient? = null
+    private var periodicHealthCheckJob: Job? = null
 
-    /**
-     * Initialize connection to the MQTT server. Should be alive when the app is alive.
-     */
+    /** Initialize connection to the MQTT server. Should be alive when the app is alive. */
     fun initialize() {
         mqttClient = MqttAndroidClient(context, SERVER_URI, UUID.randomUUID().toString())
-        mqttClient.setCallback(this)
-        mqttClient.connect(MqttConnectOptions().apply { isAutomaticReconnect = true })
+        mqttClient?.let {
+            it.setCallback(this)
+            it.connect(MqttConnectOptions().apply { isAutomaticReconnect = true })
+        }
     }
 
     fun destroy() {
-        mqttClient.disconnect()
-        if (periodicHealthCheckJob.isActive) {
-            periodicHealthCheckJob.cancel()
-        }
+        mqttClient?.disconnect()
+
+        periodicHealthCheckJob?.let { if (it.isActive) it.cancel() }
     }
 
-    /**
-     * Call this method if you bind a new sensor to your app
-     */
-    fun subscribe(sensor: SensorEntity) {
-//    sensors.add(sensor) // Make sensors an arraylist
-        mqttClient.subscribe(SENSOR_STATUS_TOPIC_PREFIX + sensor.deviceId, 1)
+    /** Call this method if you bind a new sensor to your app */
+    private fun subscribe(sensor: SensorEntity) {
+        mqttClient?.subscribe(SENSOR_STATUS_TOPIC_PREFIX + sensor.deviceId, 1)
     }
 
     fun healthCheck(sensor: SensorEntity) {
-        if (mqttClient.isConnected) {
-            val preferences =
-                context.getSharedPreferences(SettingsActivity.PREFERENCES_FILE, Context.MODE_PRIVATE)
-            val token = preferences.getString(SettingsActivity.PREFERENCES_FIREBASE_TOKEN, "")
-            val msg = MqttMessage("{\"token\": \"$token\"}".toByteArray())
-            mqttClient.publish(SENSOR_HEALTH_TOPIC_PREFIX + sensor.deviceId, msg)
+        mqttClient?.let { client ->
+            if (!client.isConnected) return
+
+            val token = getSharedPreferences(context).getString(PREFERENCES_FIREBASE_TOKEN, "")
+            client.publish(SENSOR_HEALTH_TOPIC_PREFIX + sensor.deviceId,
+                           MqttMessage("{\"token\": \"$token\"}".toByteArray()))
+
             Log.i(TAG, "Sent health check msg to ${sensor.name} with token $token")
 
-            LocalSensorDb.getInstance(context)
-                .getSensorDao()
-                .updateSensorLastHealthCheck(
-                    ZonedDateTime.now(),
-                    sensor.id
-                )
+            LocalSensorDb.getSensorDao(context)
+                    .updateSensorLastHealthCheck(ZonedDateTime.now(), sensor.id)
         }
     }
 
-    fun healthCheckAllSensors() {
-        sensors.forEach {
-            healthCheck(it)
-        }
-    }
+    fun healthCheckAllSensors() = sensors.forEach { healthCheck(it) }
 
     fun turnOnOff(sensor: SensorEntity) {
-        val activityStatus = if (sensor.turnedOn) 1 else 0
-        val msg = MqttMessage("{active: $activityStatus}".toByteArray())
-        mqttClient.publish(SENSOR_CONFIG_TOPIC_PREFIX + sensor.deviceId, msg)
-        Log.i(TAG, "Turned on/off sensor ${sensor.name}")
+        mqttClient?.let { client ->
+            if (!client.isConnected) return
+
+            val activityStatus = if (sensor.turnedOn) 1 else 0
+            client.publish(SENSOR_CONFIG_TOPIC_PREFIX + sensor.deviceId,
+                           MqttMessage("{active: $activityStatus}".toByteArray()))
+
+            Log.i(TAG, "Turned on/off sensor ${sensor.name}")
+        }
     }
 
     fun initializePeriodicHealthCheck() {
         periodicHealthCheckJob = CoroutineScope(Dispatchers.Default).launch {
             while (true) {
                 healthCheckAllSensors()
-                context.sendBroadcast(
-                    Intent(STATUS_REQUEST_ACTION)
-                )
+                context.sendBroadcast(Intent(STATUS_REQUEST_ACTION))
                 delay(HEALTH_HEARTBEAT_INTERVAL)
             }
         }
-        periodicHealthCheckJob.start()
+
+        periodicHealthCheckJob?.start()
     }
 
-    /**
-     * Either connected or reconnected. The given sensors' status topics are subscribed to.
-     */
+    /** Either connected or reconnected. The given sensors' status topics are subscribed to. */
     override fun connectComplete(reconnect: Boolean, serverURI: String?) {
         Log.i(TAG, "MQTT Connected")
-        for (sensor in sensors) {
-            subscribe(sensor)
-        }
+
+        for (sensor in sensors) subscribe(sensor)
+
         healthCheckAllSensors()
     }
 
     override fun messageArrived(topic: String?, message: MqttMessage?) {
-        Log.i(TAG, "MQTT Message: `${message.toString()}` (topic=`${topic}`)")
+        Log.i(TAG, "MQTT Message: \"${message.toString()}\" (topic=\"${topic}\")")
 
-        if (topic != null) {
-            val deviceId = topic.split("/").last()
-            val sensor = LocalSensorDb.getSensorDao(context).findSensorByDeviceId(deviceId)
-            if (sensor != null) {
-                val json = Gson().fromJson(message.toString(), JsonObject::class.java)
-                sensor.turnedOn = json.get("active").asBoolean
-                sensor.successfulHealthCheck = ZonedDateTime.now()
-                LocalSensorDb.getSensorDao(context).updateSensor(sensor)
+        if (topic == null) return
 
-                context.sendBroadcast(
-                    Intent(STATUS_RESPONSE_ACTION)
-                        .putExtra("deviceId", deviceId)
-                        .putExtra("message", message.toString())
-                )
-            }
-        }
+        val deviceId = topic.split("/").last()
+        val sensor = LocalSensorDb.getSensorDao(context).findSensorByDeviceId(deviceId) ?: return
+
+        val json = Gson().fromJson(message.toString(), JsonObject::class.java)
+        sensor.turnedOn = json.get("active").asBoolean
+        sensor.successfulHealthCheck = ZonedDateTime.now()
+        LocalSensorDb.getSensorDao(context).updateSensor(sensor)
+
+        context.sendBroadcast(Intent(STATUS_RESPONSE_ACTION).apply {
+            putExtra("deviceId", deviceId)
+            putExtra("message", message.toString())
+        })
     }
 
     override fun connectionLost(cause: Throwable?) {
