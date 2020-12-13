2ed74b762e00d166e318afd3f40e293277a0b408
Kert Tali <kert@astri.ee>
2020-12-14 01:26:05 +0200

    Modify notification's intent so clicking it sends to the detail view of the sensor.

--- app/src/main/java/li/kta/espguard/FirebaseService.kt
+++ app/src/main/java/li/kta/espguard/FirebaseService.kt
@@ -3,6 +3,7 @@ package li.kta.espguard
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
@@ -11,7 +12,7 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import li.kta.espguard.activities.MainActivity
+import li.kta.espguard.activities.SensorDetailsActivity
 import li.kta.espguard.activities.SettingsActivity
 import li.kta.espguard.activities.SettingsActivity.Companion.PREFERENCES_IGNORE_NOTIFICATIONS
 import li.kta.espguard.activities.SettingsActivity.Companion.PREFERENCES_QUIET_NOTIFICATIONS
@@ -19,6 +20,7 @@ import li.kta.espguard.activities.SettingsActivity.Companion.getBooleanPreferenc
 import li.kta.espguard.activities.SettingsActivity.Companion.getSharedPreferences
 import li.kta.espguard.room.EventEntity
 import li.kta.espguard.room.LocalSensorDb
+import li.kta.espguard.room.SensorEntity
 import java.time.Instant
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -70,7 +72,8 @@ class FirebaseService : FirebaseMessagingService() {
         if (!ignoreNotifications())
             sendNotification(resources.getString(R.string.notification_text_template,
                                                  sensor.name,
-                                                 event.eventTime?.let { formattedDate(it) }))
+                                                 event.eventTime?.let { formattedDate(it) }),
+              sensor)
 
         sendBroadcast(Intent(STATUS_RESPONSE_ACTION))
 
@@ -90,11 +93,8 @@ class FirebaseService : FirebaseMessagingService() {
     private fun ignoreNotifications(): Boolean =
             getBooleanPreference(this, PREFERENCES_IGNORE_NOTIFICATIONS)
 
-    /** TODO how to get this to send the user to the details view when clicking the notification
-     * https://github.com/firebase/quickstart-android/blob/master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/MyFirebaseMessagingService.kt
-     */
-    private fun sendNotification(message: String) {
-        val notificationBuilder = buildNotification(message)
+    private fun sendNotification(message: String, sensor: SensorEntity) {
+        val notificationBuilder = buildNotification(message, sensor)
 
         putNotificationsOnQuiet(notificationBuilder)
 
@@ -110,15 +110,14 @@ class FirebaseService : FirebaseMessagingService() {
         notificationManager.notify(0 /* auto-generated */, notificationBuilder.build())
     }
 
-    private fun buildNotification(message: String): NotificationCompat.Builder {
-        val intent = Intent(this, MainActivity::class.java)
-                .apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) }
+    private fun buildNotification(message: String, sensor: SensorEntity): NotificationCompat.Builder {
+        val intent = Intent(this, SensorDetailsActivity::class.java)
+          .apply { putExtra(SensorDetailsActivity.EXTRA_SENSOR_ID, sensor.id) }
 
-        val pendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                intent,
-                PendingIntent.FLAG_ONE_SHOT)
+        val pendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+          addNextIntentWithParentStack(intent)
+          getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
 
         return NotificationCompat.Builder(this, NOTIFICATIONS_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_eye_24)
